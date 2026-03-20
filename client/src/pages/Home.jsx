2c6cf524eebd0b4f16aa4aed0417c567eada5cd3
Tushar Gupta <tushargupta8954@gmail.com>
2026-03-20 23:47:09 +0530

    Created the Testimonials page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/home/Banner'
 import Hero from '../components/home/Hero'
 import Features from '../components/home/Features'
+import Testimonials from '../components/home/Testimonials'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner/> 
       <Hero/>
       <Features/>
+      <Testimonials/>
     </div>
   )
 }
