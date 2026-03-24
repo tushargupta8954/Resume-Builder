18b171f225fcd2a11aae04f14e809c497d1a9635
Tushar Gupta <tushargupta8954@gmail.com>
2026-03-25 00:02:47 +0530

    Created the Navbar for user's Dashboard

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const Layout = () => {
   return (
     <div>
-      <h1>Layout page</h1>
-      <div>
+      
+      <div className='min-h-screen bg-gray-50'>
+        <Navbar/>
         <Outlet/>
+
       </div>
     </div>
   )
