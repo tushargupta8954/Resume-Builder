b67271ac4a57f7d2f0dfb328bf9ad023d45a63e1
Tushar Gupta <tushargupta8954@gmail.com>
2026-03-31 23:10:43 +0530

    Build the template options in resume builder

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -4,6 +4,7 @@ import { dummyResumeData } from '../assets/assets'
 import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User, UserIcon } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
+import TemplateSelector from '../components/TemplateSelector'
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams()
@@ -65,20 +66,24 @@ const ResumeBuilder = () => {
               transition-all duration-2000' style={{width: `${activeSectionIndex * 100 / (sections.length -1)}%`}} />
               {/* Section Navigation */}
               <div  className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'> 
-                <div>
-                  <div></div>
-                  <div className='flex items-center'>
-                    {activeSectionIndex !==0 &&(
-                      <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.max(prevIndex - 1,0))} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all' disabled={activeSectionIndex}>
-                        <ChevronLeft className='size-4'/>Previous
-                      </button>
-                    )}
-                    <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.min(prevIndex + 1, sections.length - 1))} className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} disabled={activeSectionIndex === sections.length - 1}>
-                        Next <ChevronRight className='size-4'/>
-                      </button>
-
-                  </div>
+              
+                <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
+                   <TemplateSelector selectedTemplate={resumeData.template} onChange={(template)=> setResumeData(prev => ({...prev, template}))} />
                 </div>
+
+
+                <div className='flex items-center'>
+                  {activeSectionIndex !==0 &&(
+                    <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.max(prevIndex - 1,0))} className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all' disabled={activeSectionIndex}>
+                      <ChevronLeft className='size-4'/>Previous
+                    </button>
+                  )}
+                  <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.min(prevIndex + 1, sections.length - 1))} className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} disabled={activeSectionIndex === sections.length - 1}>
+                      Next <ChevronRight className='size-4'/>
+                    </button>
+
+                </div>
+                
 
               </div>
 
