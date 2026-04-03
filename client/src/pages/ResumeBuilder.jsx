0163c2f4397db6bdd54ca8c12a28ae16c15785f6
Tushar Gupta <tushargupta8954@gmail.com>
2026-04-03 15:58:35 +0530

    Created the Project component in resume builder page

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -9,6 +9,7 @@ import ColorPicker from '../components/ColorPicker'
 import ProfessionalSummaryForm from '../components/ProfessionalSummaryForm'
 import ExperienceForm from '../components/ExperienceForm'
 import EducationForm from '../components/EducationForm'
+import ProjectForm from '../components/ProjectForm'
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams()
@@ -110,6 +111,10 @@ const ResumeBuilder = () => {
                 
                  {activeSection.id === 'education' && (
                   <EducationForm data={resumeData.education} onChange={(data)=> setResumeData(prev=> ({...prev, education: data}))}  />
+                )}
+
+                {activeSection.id === 'projects' && (
+                  <ProjectForm data={resumeData.project} onChange={(data)=> setResumeData(prev=> ({...prev, project: data}))}  />
                 )}
 
               </div>
