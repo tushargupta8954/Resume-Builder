92ef54d80cd28b3f485dd43c9bc4b83e93cce501
Tushar Gupta <tushargupta8954@gmail.com>
2026-04-06 16:16:05 +0530

    Created resume schema

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import protect from "../middlewares/authMiddleware.js";
+import { getUserById, getUserResumes, loginUser, registerUser } from "../controllers/UserController.js";
 
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
-userRouter.post('/data', protect, getUserId);
+userRouter.post('/data', protect, getUserById);
+userRouter.post('/resumes', protect, getUserResumes);
+
 
 export default userRouter;
